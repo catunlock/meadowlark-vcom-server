4a47fa3292642a3e0fdc29db5b83eb2ff1a36f72
Alberto López Sánchez <alopez@cells.es>
2021-02-22 15:39:29 +0100

    impl read and write traits

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate libc;
 use libc::{c_uchar, c_uint, c_ulong, c_ulonglong, size_t};
-use std::{ffi::{ CString, CStr}, io::{BufRead, Write}};
+use std::{ffi::{ CString, CStr}, io::{BufRead, Read, Write}};
 use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufWriter};
+use std::io::{BufReader, LineWriter, Result};
 
 #[link(name = "usbdrvd")]
 extern {
@@ -23,76 +23,93 @@ extern {
 }
 
 struct Meadowlark{
-    usb_pid: u32,
-    flagsandattrs: u32,
-    guid: [u8;16],
-
     _dev1: u64,
     _pipe0: u64,
-    _pipe1: u64
+    _pipe1: u64,
+
+    opened: bool
 }
 
 impl Meadowlark {
     fn new(usb_pid: u32, guid: [u8;16]) -> Self {
-        Self{usb_pid: usb_pid, flagsandattrs: 0x40000000, guid: guid, _dev1:0, _pipe0: 0, _pipe1: 1}
+        let flagsandattrs = 0x40000000;
+        let _dev1 =  unsafe{USBDRVD_OpenDevice(1, flagsandattrs, usb_pid)};
+        let _pipe0 = unsafe{USBDRVD_PipeOpen(1, 0, flagsandattrs, guid.as_ptr())};
+        let _pipe1 = unsafe{USBDRVD_PipeOpen(1, 1, flagsandattrs, guid.as_ptr())};
+
+        println!("dev1: {}, pipe0: {}, pipe1: {}", _dev1, _pipe0, _pipe1);
+        Self{_dev1: _dev1, _pipe0: _pipe0, _pipe1: _pipe1, opened: false}
     }
 
-    fn open(&mut self) {
-        self._dev1 =  unsafe{USBDRVD_OpenDevice(1, self.flagsandattrs, self.usb_pid)};
-        self._pipe0 = unsafe{USBDRVD_PipeOpen(1, 0, self.flagsandattrs, self.guid.as_ptr())};
-        self._pipe1 = unsafe{USBDRVD_PipeOpen(1, 1, self.flagsandattrs, self.guid.as_ptr())};
-        println!("dev1: {}, pipe0: {}, pipe1: {}", self._dev1, self._pipe0, self._pipe1);
+    fn close(&mut self) {
+        if self.opened {
+            unsafe {USBDRVD_PipeClose(self._pipe0)};
+            unsafe {USBDRVD_PipeClose(self._pipe1)};
+            unsafe {USBDRVD_CloseDevice(self._dev1)};
+            self.opened = false;
+        }
     }
 
-    fn write(&self, buf: &str, size_r: usize) {
-        let c_buf = CString::new(buf).expect("Error creating C String.");
-        
-        println!("Recv: {:?}, size: {}", c_buf, size_r);
-        unsafe {USBDRVD_BulkWrite(self._dev1, 1, c_buf.as_ptr() as *const u8, size_r as u32)};
-    }
+}
 
-    fn read_into(&self, buf: &mut String) -> usize {
-        let size_r = unsafe {USBDRVD_BulkRead(self._dev1, 0, buf.as_bytes_mut().as_ptr(),
-            buf.as_bytes().len() as u64)} as usize;
-        return size_r;
-    }
-
-    fn close(&self) {
-        unsafe {USBDRVD_PipeClose(self._pipe0)};
-        unsafe {USBDRVD_PipeClose(self._pipe1)};
-        unsafe {USBDRVD_CloseDevice(self._dev1)};
+impl Read for &Meadowlark {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let size_r = unsafe {USBDRVD_BulkRead(self._dev1, 0, buf.as_ptr(),buf.len() as u32)} as usize;
+        return Ok(size_r);
     }
 }
+
+impl Write for &Meadowlark {
+    
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        let c_buf = CString::new(buf).expect("Error creating C String.");
+        
+        println!("Recv: {:?}, size: {}", c_buf, buf.len());
+        unsafe {USBDRVD_BulkWrite(self._dev1, 1, c_buf.as_ptr() as *const u8, buf.len() as u32)};
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl Drop for Meadowlark {
+
+    fn drop(&mut self) {
+        self.close();
+    }
+}
+    
+
+    
 
 const USB_PID: u32 = 0x139C;
 const GUID: [u8;16] = [0xa2 ,0x2b ,0x5b ,0x8b ,0xc6 ,0x70 ,0x41 ,0x98 ,0x2b ,0x7d ,0xfc ,0x9d ,0xba ,0xaa ,0x85 ,0x95];
 
 fn handle_client(stream: TcpStream) {
 
+    let meadowlark = Meadowlark::new(USB_PID, GUID);  
 
-    let mut meadowlark = Meadowlark::new(USB_PID, GUID);
-    meadowlark.open();    
+    let mut tcp_reader = BufReader::new(&stream);
+    let mut tcp_writer = LineWriter::new(&stream);
 
-    let mut reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
-
-    let mut buf = String::with_capacity(1024);
+    let mut usb_reader = BufReader::new(&meadowlark);
+    let mut usb_writter = LineWriter::new(&meadowlark);
 
     loop {
-        match reader.read_line(&mut buf) {
+        let mut buf = String::with_capacity(1024);
+        match tcp_reader.read_line(&mut buf) {
             Ok(size_r) => {
-
-                meadowlark.write(&buf, size_r);
-                let size_r = meadowlark.read_into(&mut buf);
                 
-                println!("Write: {}", buf);
-                writer.write_all(&mut buf.as_bytes());
-                writer.flush().expect("Error flushing the write buffer.");
+                usb_writter.write_all(&buf.as_bytes());
+                usb_reader.read_line(&mut buf);
+                
+                tcp_writer.write_all(&mut buf.as_bytes());
             }
             Err(e) => {
                 println!("Error reading from the buffer: {}", e);
-                meadowlark.close();
-
                 break;
             }
         }
